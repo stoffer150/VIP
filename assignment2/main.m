6b98b11efae4407ca34546a2308cc46f10134b62
Unknown <stoffer@amnesi.dk>
2017-12-06 23:24:51 +0100

    Bare en lille opdatering til main

--- assignment2/main.m
+++ assignment2/main.m
@@ -1,12 +1,19 @@
 in_path = 'input/';
 out_path = 'output/';
-file = {'Img001_diffuse_smallgray', '.png'};
+files =  [{'Img001_diffuse_smallgray'; '.png'}, ...
+         {'Img002_diffuse_smallgray'; '.png'}, ...
+         {'Img009_diffuse_smallgray'; '.png'}];
 
-I = imread([in_path, file{1}, file{2}]);
-points = Harris_corner_function(I, 0.5, 15, 0.005 );
+% PART 1
+for file = files
+    I = imread([in_path, file{1}, file{2}]);
+    points = Harris_corner_function(I, 1, 25, 0.01 );
 
-output = place_markers(I, points);
+    output = place_markers(I, points);
 
-imshow(output);
+    figure();
+    imshow(output);
+    imwrite(output,[out_path, file{1}, file{2}])
+end
 
-%imsave(output, [out_path, file{1}, file{2}]);
+% PART 2
