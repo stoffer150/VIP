ec4f312534da2f18b515f2e19b9f0677b71930e3
stoffer150 <chrisbelhage@gmail.com>
2017-12-06 15:52:54 +0100

    Bare en lille main

--- assignment2/main.m
+++ assignment2/main.m
@@ -1,1 +1,10 @@
-Harris_corner_function( im, sigma, sz, t )
+in_path = 'input/';
+out_path = 'output/';
+file = {'Img001_diffuse_smallgray', '.png'};
+
+I = imread([in_path, file{1}, file{2}]);
+output = Harris_corner_function(I, 1, 10, 0.5 );
+
+imshow(output);
+
+saveas(output, [out_path, file{1}, file{2}]);
