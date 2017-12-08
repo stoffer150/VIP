176f72d460c16f0c1f27ca9388ae04e5759e75c0
Avalynbyspirit <bqk662@alumni.ku.dk>
2017-12-08 15:51:06 +0100

    Added normalized descriptors.

---
+++ assignment2/extract2.m
@@ -0,0 +1,27 @@
+function [ descriptors ] = extract( im, points, n )
+
+% Read in image
+I = imread(im);
+I = double(I);
+
+% Pad image with zeros
+[row, col] = size(I);
+im_pad = zeros(row + 2*n, col + 2*n);
+im_pad(n+1:row+n,n+1:col+n) = I;
+
+% Shift points to match padding
+points_shift = points + n;
+
+% Compute descriptors
+steps = floor(n / 2);
+descriptors = zeros(n*n,size(points,1));
+
+for i = 1:size(points,1)
+    
+    p = points_shift(i,:);
+    tmp = reshape(im_pad(p(2)-steps : p(2) + steps, p(1)-steps : p(1) + steps), [n*n,1]);
+    descriptors(:,i) = tmp / norm(tmp);
+    
+end
+
+end
