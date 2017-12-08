360e016efd93b56644dbfbca4157ab91c130f3c0
stoffer150 <chrisbelhage@gmail.com>
2017-12-08 15:32:11 +0100

    Two way comparison added

--- assignment2/find_matchings.m
+++ assignment2/find_matchings.m
@@ -5,11 +5,27 @@ function [best_match] = find_matchings(d1, d2, thres)
     d1_tensor = repelem(d1, 1, 1, n_d2);
     d2_tensor = repelem(d2, 1, 1, n_d1);
     
+    %From d1 to d2
+    
     diff = sum((d1_tensor - permute(d2_tensor, [1, 3, 2])).^2, 1);
     
     [min_diff, idx] = min(diff, [], 3);
     
-    best_match = [1:n_d1; idx; min_diff];
+    match1 = [1:n_d1; idx; min_diff];
+    
+    %From d2 to d1
+    
+    diff = sum((d2_tensor - permute(d1_tensor, [1, 3, 2])).^2, 1);
+    
+    [min_diff, idx] = min(diff, [], 3);
+    
+    match2 = [1:n_d1; idx; min_diff];
+    
+    %Do a two way comparison
+    
+    best_match = match1(:,match2(2,match1(2,:)) == 1:n_d1);
+    
+    %Filter bad similarities
     
     best_match(:,best_match(3,:) > thres) = [];
     
