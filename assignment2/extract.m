17a6fcb9f61c20ad4c0446a17d4f0d45dedb90a7
Avalynbyspirit <bqk662@alumni.ku.dk>
2017-12-08 15:10:33 +0100

    Commented descriptor extraction function.

--- assignment2/extract.m
+++ assignment2/extract.m
@@ -1,13 +1,18 @@
 function [ descriptors ] = extract( im, points, n )
 
+% Read in image
 I = imread(im);
 I = double(I);
 
+% Pad image with zeros
 [row, col] = size(I);
 im_pad = zeros(row + 2*n, col + 2*n);
 im_pad(n+1:row+n,n+1:col+n) = I;
+
+% Shift points to match padding
 points_shift = points + n;
 
+% Compute descriptors
 steps = floor(n / 2);
 descriptors = zeros(n*n,size(points,1));
 
