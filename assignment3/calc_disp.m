3e26e21534421e17d4ebfd3e2ee8753a70704fe2
stoffer150 <chrisbelhage@gmail.com>
2018-01-05 16:47:52 +0100

    Optimizations

--- assignment3/calc_disp.m
+++ assignment3/calc_disp.m
@@ -1,28 +1,62 @@
-function disp = calc_disp(im1,im2,s,pre_disp)
+function dispa = calc_disp(im1,im2,s,pre_disp)
     if nargin < 4
-        pre_disp = zeros(size(im1));
+        pre_disp = zeros(size(im1), 'uint8');
     end
-    disp = zeros(size(im1));
+    dispa = zeros(size(im1), 'uint8');
     
     [row,col] = size(im1);
     h_pad = floor(s(1)/2);
     v_pad = floor(s(2)/2);
-    im_pad = zeros(row+h_pad*2,col+v_pad*2);
-    im_pad(h_pad+1:h_pad+row,v_pad+1:v_pad+col) = im1;
-    im1 = im_pad;
-    im_pad(h_pad+1:h_pad+row,v_pad+1:v_pad+col) = im2;
-    im2 = im_pad;
     
-    for i = h_pad + 1:h_pad+row
-        for j = v_pad + 1:v_pad+col
-            d = pre_disp(i - h_pad,j - v_pad);
-            temp = im1(i-h_pad : i+h_pad,j-v_pad :j+v_pad);
-            A = im2(i-h_pad :i+h_pad,:);
-            corr = abs(normxcorr2(temp,A));
-            c_sz = size(corr);
-            max_ind = find(corr(ceil(c_sz(1)/2),:) == max(corr(ceil(c_sz(1)/2),:)));
-            disp(i - h_pad,j - v_pad) = max_ind(1) - j - h_pad + d;
+    for i = 1:row
+        disp([int2str(i)])
+        for j = 1:col
+
+            d = uint64(pre_disp(i,j));
+            
+            %Calculate template
+            temp = im1(max(i-v_pad, 1):min(i+v_pad, row), ...
+                       max(j-h_pad, 1):min(j+h_pad, col));
+            temp = double(temp) + rand(size(temp))*0.01;
+            temp_center = [i - max(i-v_pad, 1) + 1, j - max(j-h_pad, 1) + 1];
+            
+            %Scale of A field
+            a_scale = 2;
+            
+            %Calculate x coordinates for use in A
+            x_idx = max(j-d*2-h_pad*a_scale, 1):max(min([j-d*2+h_pad*a_scale, j+h_pad, col]), size(temp,2)+h_pad);
+            
+            %Calculate A
+            A = im2(max(i-v_pad, 1):min(i+v_pad, row),x_idx);
+            A_center = double([temp_center(1), min(d*2+v_pad*a_scale, j - 1) + 1]);
+            
+            %Calclate normalized correlation
+            corr = normxcorr2(temp,A);
+            corr_center = A_center + size(temp) - temp_center;
+            
+            %Clip it to only relevant matches
+            corr = corr(corr_center(1), h_pad*2:end-h_pad);
+            
+            %Find maximum index;
+            max_ind = find(corr == max(corr));
+            max_ind = max_ind(1);
+            
+%             if max_ind == 1
+%                 max_ind = find(corr == max(corr(2:end)));
+%             end
+            
+            %Calculate the difference in pixel-position
+            diff = corr_center(2) - (max_ind + h_pad*2);
+
+            %Special case
+            if diff < 0
+               max_ind = find(corr == max(corr(1:end-1)));
+               max_ind = max_ind(1);
+               diff = corr_center(2) - (max_ind + h_pad*2);
+            end
+            
+            %Store result
+            dispa(i,j) = diff;
         end
     end
-    
 end
--- assignment3/main.m
+++ assignment3/main.m
@@ -1,12 +1,11 @@
 close all
 clear
 
-first_im = double(rgb2gray(imread('tsukuba/scene1.row3.col1.ppm')));
-second_im = double(rgb2gray(imread('tsukuba/scene1.row3.col2.ppm')));
+first_im = rgb2gray(imread('tsukuba/scene1.row3.col1.ppm'));
+second_im = rgb2gray(imread('tsukuba/scene1.row3.col2.ppm'));
 
-noise = 0.01*rand(size(first_im));
-first_im = first_im + noise;
-second_im = second_im + noise;
+first_im = imgaussfilt(first_im, 0.01);
+second_im = imgaussfilt(second_im, 0.01);
 
 imsize = min(size(first_im, 1), size(second_im, 2));
 
@@ -22,32 +21,33 @@ scales{1} = {first_im, second_im};
 prev1 = first_im;
 prev2 = second_im;
 
+disp('Making pyramids...')
 for level = 2:num_scales
     prev1 = impyramid(prev1,'reduce');
     prev2 = impyramid(prev2,'reduce');
     scales{level} = {prev1, prev2};
 end
-
+disp('Calculating dispartities...')
 for level = num_scales:-1:1
     im1 = scales{level}{1};
     im2 = scales{level}{2};
-    
+    disp(level)
     if level < num_scales
         disparities{level} = ...
-            calc_disp(im1, im2, [5, 5], imresize(disparities{level+1}, 2, 'nearest'));
+            calc_disp(im1, im2, [7, 7], imresize(disparities{level+1}, 2, 'nearest'));
     else
         disparities{level} = ...
-            calc_disp(im1, im2, [5, 5]);
+            calc_disp(im1, im2, [7, 7]);
     end
 end
 
 gt_disp = imread('tsukuba/truedisp.row3.col3.pgm');
 
-
-disparity = disparities{1};
+disp('Calculating statistics...')
+disparity = disparities{1,1};
 disparity = disparity - min(disparity);
-disparity = uint8(disparity ./ max(disparity) * 255);
-err = abs((disparity - gt_disp));
+disparity = double(disparity) ./ double(max(disparity(:)));
+err = abs((disparities{1,1} - gt_disp));
 err = err(:);
 
 mean_err = mean(err);
