acd1428a7c1829dd3d941d55c195ccafcd6683ba
Avalynbyspirit <Avalynbyspirit@hotmail.com>
2018-01-10 15:11:46 +0100

    More albedo.

--- assignment4/main.m
+++ assignment4/main.m
@@ -3,19 +3,27 @@ load('Beethoven.mat');
 
 %------------------------------------------%
 
+% Create matrix J
 num_lights = size(I, 3);
 
 J = [];
 
-for n = 1:num_lights
-    nz = I(:,:, n);
+for i = 1:num_lights
+    nz = I(:,:, i);
     nz = nz(mask);
-    J(n, :) = nz(:);
+    J(i, :) = nz(:);
 end
 
+% Compute albedo and norm field
 M = S\J;
-
 albedo = vecnorm(M);
+n = M ./ albedo;
 
-%depth_map = unbiased_integrate(M(1,:),M(2,:),M(3,:),mask);
+% Show albedo as image
+albmask = double(mask);
+albmask(albmask>0) = albedo;
+imshow(albmask,[]);
+
+% Compute depth map
+depth_map = unbiased_integrate(n(1,:),n(2,:),n(3,:),mask);
 
