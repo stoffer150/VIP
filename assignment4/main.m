f40687116bce70a573d745ce366f8fadb62ad3bd
stoffer150 <chrisbelhage@gmail.com>
2018-01-10 14:25:56 +0100

    Code so far...

--- assignment4/main.m
+++ assignment4/main.m
@@ -1,3 +1,21 @@
-load('Buddha.mat');
+%load('Buddha.mat');
 load('Beethoven.mat');
+
+%------------------------------------------%
+
+num_lights = size(I, 3);
+
+J = [];
+
+for n = 1:num_lights
+    nz = I(:,:, n);
+    nz = nz(mask);
+    J(n, :) = nz(:);
+end
+
+M = S\J;
+
+albedo = vecnorm(M);
+
+%depth_map = unbiased_integrate(M(1,:),M(2,:),M(3,:),mask);
 
