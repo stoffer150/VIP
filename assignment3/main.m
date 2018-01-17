d9e0f8e34863f9a49bdce7f03f0b45261961e192
Avalynbyspirit <Avalynbyspirit@hotmail.com>
2018-01-17 16:56:57 +0100

    Modified main.m to compute for disparities for all data sets.

--- assignment3/main.m
+++ assignment3/main.m
@@ -1,90 +1,96 @@
 close all
 clear
 
-first_im = rgb2gray(imread('tsukuba/scene1.row3.col1.ppm'));
-second_im = rgb2gray(imread('tsukuba/scene1.row3.col3.ppm'));
+first_ims = {'tsukuba/scene1.row3.col1.ppm','venus/im0.ppm','map/im0.pgm'};
+second_ims = {'tsukuba/scene1.row3.col3.ppm','venus/im2.ppm','map/im1.pgm'};
+true_disps = {'tsukuba/truedisp.row3.col3.pgm','venus/disp2.pgm','disp1.pgm'};
 
-first_im = imgaussfilt(first_im, 0.01);
-second_im = imgaussfilt(second_im, 0.01);
+for i = 1:3
+    first_im = rgb2gray(imread(first_ims(i)));
+    second_im = rgb2gray(imread(second_ims(i)));
 
-imsize = min(size(first_im, 1), size(second_im, 2));
+    first_im = imgaussfilt(first_im, 0.01);
+    second_im = imgaussfilt(second_im, 0.01);
 
-num_scales = floor(log2(imsize));
+    imsize = min(size(first_im, 1), size(second_im, 2));
 
-num_scales = min(4, num_scales);
+    num_scales = floor(log2(imsize));
 
-scales = cell(num_scales,1);
-disparities = cell(num_scales, 4, num_scales, 1);
+    num_scales = min(4, num_scales);
 
-scales{1} = {first_im, second_im};
+    scales = cell(num_scales,1);
+    disparities = cell(num_scales, 4, num_scales, 1);
 
-prev1 = first_im;
-prev2 = second_im;
+    scales{1} = {first_im, second_im};
 
-disp('Making pyramids...')
-for level = 2:num_scales
-    prev1 = impyramid(prev1,'reduce');
-    prev2 = impyramid(prev2,'reduce');
-    scales{level} = {prev1, prev2};
-end
+    prev1 = first_im;
+    prev2 = second_im;
 
-for s = 1:num_scales
-    for k = 5:2:11
-        disp('Calculating dispartities...')
-        for level = s:-1:1
-            im1 = scales{level}{1};
-            im2 = scales{level}{2};
-            disp(level)
-            if level < s
-                disparities{s, (k-5) / 2 + 1, level} = ...
-                    calc_disp(im1, im2, [k, k], repelem(disparities{s, (k-5) / 2 + 1, level+1},2,2));
-            else
-                disparities{s, (k-5) / 2 + 1, level} = ...
-                    calc_disp(im1, im2, [k, k]);
-            end
-            
-            if level == 1
-                disparity = disparities{s, (k-5) / 2 + 1, 1};
-                disparity = disparity - min(disparity(:));
-                disparity = uint8(double(disparity) ./ double(max(disparity(:))) * 255);
-                imwrite(disparity, ['disparity_s', int2str(s), '_k', int2str(k), '.png']);
+    disp('Making pyramids...')
+    for level = 2:num_scales
+        prev1 = impyramid(prev1,'reduce');
+        prev2 = impyramid(prev2,'reduce');
+        scales{level} = {prev1, prev2};
+    end
+
+    for s = 1:num_scales
+        for k = 5:2:11
+            disp('Calculating dispartities...')
+            for level = s:-1:1
+                im1 = scales{level}{1};
+                im2 = scales{level}{2};
+                disp(level)
+                if level < s
+                    disparities{s, (k-5) / 2 + 1, level} = ...
+                        calc_disp(im1, im2, [k, k], repelem(disparities{s, (k-5) / 2 + 1, level+1},2,2));
+                else
+                    disparities{s, (k-5) / 2 + 1, level} = ...
+                        calc_disp(im1, im2, [k, k]);
+                end
+                
+                if level == 1
+                    disparity = disparities{s, (k-5) / 2 + 1, 1};
+                    disparity = disparity - min(disparity(:));
+                    disparity = uint8(double(disparity) ./ double(max(disparity(:))) * 255);
+                    imwrite(disparity, ['disparity_s', int2str(s), '_k', int2str(k), '.png']);
+                end
             end
         end
     end
-end
 
-gt_disp = imread('tsukuba/truedisp.row3.col3.pgm');
-gt_disp = imadjust(gt_disp);
+    gt_disp = imread(true_disps(i));
+    gt_disp = imadjust(gt_disp);
 
-disp('Calculating statistics...')
-for n = num_scales:-1:1
-    disparity = disparities{num_scales, 2, n, 1};
-    disparity = disparity - min(disparity(:));
-    disparity = uint8(double(disparity) ./ double(max(disparity(:))) * 255);
-    imwrite(disparity, ['disparity', int2str(n), '.png']);
-    figure();
-    imshow(disparity);
-end
+    disp('Calculating statistics...')
+    for n = num_scales:-1:1
+        disparity = disparities{num_scales, 2, n, 1};
+        disparity = disparity - min(disparity(:));
+        disparity = uint8(double(disparity) ./ double(max(disparity(:))) * 255);
+        imwrite(disparity, ['disparity', int2str(n), '.png']);
+        figure();
+        imshow(disparity);
+    end
 
-disp('Calculating...')
-for k = 5:2:11
-    for s = 1:4
-        dispar = double(imadjust(disparities{s, (k-5) / 2 + 1,1}));
-        disp(strcat('k = ',num2str(k), ', scales used = ',num2str(s)))
-        err = abs(double(gt_disp) - dispar);
-        %imwrite(err, ['err_s', int2str(s), '_k', int2str(k), '.png']);
-        err = err(:);
+    disp('Calculating...')
+    for k = 5:2:11
+        for s = 1:4
+            dispar = double(imadjust(disparities{s, (k-5) / 2 + 1,1}));
+            disp(strcat('k = ',num2str(k), ', scales used = ',num2str(s)))
+            err = abs(double(gt_disp) - dispar);
+            %imwrite(err, ['err_s', int2str(s), '_k', int2str(k), '.png']);
+            err = err(:);
 
-        mean_err = mean(err);
-        disp(strcat('Mean error: ', num2str(mean_err)))
+            mean_err = mean(err);
+            disp(strcat('Mean error: ', num2str(mean_err)))
 
-        err_std = std(double(err));
-        disp(strcat('Standard deviation: ', num2str(err_std)))
+            err_std = std(double(err));
+            disp(strcat('Standard deviation: ', num2str(err_std)))
 
-        num_large_errs = size(err(err >= 3), 1);
-        disp(strcat('Number of large errors: ', num2str(num_large_errs)))
+            num_large_errs = size(err(err >= 3), 1);
+            disp(strcat('Number of large errors: ', num2str(num_large_errs)))
 
-        frac_large_errs = num_large_errs / size(err, 1);
-        disp(strcat('Faction of large errors: ', num2str(frac_large_errs)))
+            frac_large_errs = num_large_errs / size(err, 1);
+            disp(strcat('Faction of large errors: ', num2str(frac_large_errs)))
+        end
     end
 end
